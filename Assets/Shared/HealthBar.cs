78c3f2237ffed1178b2913f98cc993c1f37ab663
CamD67 <7773345+camd67@users.noreply.github.com>
2022-02-20 21:40:58 -0800

    Hide healthbars when full (but not for player)

--- Assets/Shared/HealthBar.cs
+++ Assets/Shared/HealthBar.cs
@@ -1,4 +1,3 @@
-using System;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -18,16 +17,31 @@ namespace Shared
         [SerializeField]
         private Image healthBarSprite;
 
+        [SerializeField]
+        private Image healthBarBackgroundSprite;
+
+        [SerializeField, Tooltip("Hides the healthbar if at 100%")]
+        private bool hideAtFull = true;
+
         private Camera cam;
 
         private void Start()
         {
             cam = Camera.main;
+            if (hideAtFull && Mathf.Approximately(current, 1))
+            {
+                healthBarSprite.enabled = false;
+                healthBarBackgroundSprite.enabled = false;
+            }
+        }
+
+        private void Update()
+        {
+            healthBarSprite.fillAmount = current;
         }
 
         private void LateUpdate()
         {
-            healthBarSprite.fillAmount = current;
 
             var t = transform;
             t.rotation = cam.transform.rotation;
@@ -37,11 +51,21 @@ namespace Shared
         public void SetCurrent(float current)
         {
             this.current = Mathf.Clamp01(current);
+            if (hideAtFull && Mathf.Approximately(this.current, 1))
+            {
+                healthBarSprite.enabled = false;
+                healthBarBackgroundSprite.enabled = false;
+            }
+            else
+            {
+                healthBarSprite.enabled = true;
+                healthBarBackgroundSprite.enabled = true;
+            }
         }
 
         public void ComputeCurrent(float current, float max)
         {
-            this.current = Mathf.Clamp01(current / max);
+            SetCurrent(current / max);
         }
     }
 }
